3eae0520d309d76fd59c438b666e865f35af1b2c
Chris Petrula <cpetrula@yahoo.com>
2025-12-26 14:19:15 -0800

    add front-end build files

--- frontend/dist/assets/index-CMdrEfz9.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:frontend/dist/assets/index-CMdrEfz9.js
-import{s as e}from"./index-CewzQF7O.js";var t={name:"Dropdown",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Select component instead.")}};export{t as s};
-========
-import{s as e}from"./index-DQOFjhQM.js";var t={name:"Dropdown",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Select component instead.")}};export{t as s};
->>>>>>>> 61f62e3 (add front-end build files):frontend/dist/assets/index-dJWyEVQ7.js
