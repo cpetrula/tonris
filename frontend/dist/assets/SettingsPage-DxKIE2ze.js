8599d5bac3c929cc5c6fb1eaaf4df375ef89d3b1
Chris Petrula <cpetrula@yahoo.com>
2025-12-26 14:28:04 -0800

    add front-end build files

--- frontend/dist/assets/SettingsPage-DxKIE2ze.js
+++ frontend/dist/assets/SettingsPage-DxKIE2ze.js
@@ -1,4 +1,4 @@
-import{B as Y,aR as q,M as K,c as v,o as r,a as t,C as B,H as W,d as Z,aS as J,i as w,j as Q,k as M,l as S,e as a,w as u,u as s,m as X,f as _,t as P,s as V,q as x,g as k,F as ee,r as te,x as le}from"./index-gINyIjrw.js";import{s as N}from"./index-BuQ4ULPP.js";import{s as A}from"./index-Basdmm0D.js";import{s as se,a as U}from"./index-D_GAdTrm.js";import"./index-f5urNBnX.js";import"./index-BLe3RfKs.js";import"./index-BVcQbiWG.js";var ae=`
+import{B as Y,aR as q,M as K,c as v,o as r,a as t,C as B,H as W,d as Z,aS as J,i as w,j as Q,k as M,l as S,e as a,w as u,u as s,m as X,f as _,t as P,s as V,q as x,g as k,F as ee,r as te,x as le}from"./index-Bb9OieXq.js";import{s as N}from"./index-2zwC8oqZ.js";import{s as A}from"./index-CMdrEfz9.js";import{s as se,a as U}from"./index-0nejMClq.js";import"./index-CewzQF7O.js";import"./index-DShqLUB9.js";import"./index-CtcGsKRH.js";var ae=`
     .p-toggleswitch {
         display: inline-block;
         width: dt('toggleswitch.width');
